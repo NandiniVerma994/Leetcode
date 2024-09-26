b5524dd29e3958165e52f62a004602952986651c
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2024-09-26 17:43:34 +0530

    Time: 4 ms (93.78%), Space: 14.6 MB (18.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        /*int n = nums.size();
         int l = 0;
         int r = n-1;
         vector<int> sorted = nums;
@@ -20,5 +20,19 @@ public:
         }
 
         return result;
+    }*/
+    int n = nums.size();
+    map<int,int> mpp;
+    for(int i=0; i<n; i++) {
+        int num = nums[i];
+        int moreNeeded = target - num;
+        if(mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        else {
+            mpp[num] = i;
+        }
+    }
+    return {};
     }
 };
