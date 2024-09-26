41548dfd13604067b709b885bbc2ae62faeedfb3
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2024-09-26 17:18:20 +0530

    Time: 7 ms (85.22%), Space: 13.5 MB (52.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        /*int n = nums.size();
-        int left = 0, right = n-1;
-        sort(nums.begin(), nums.end());
-        while(left < right) {
-            int sum = nums[left] + nums[right];
-            if(sum == target) return {left, right};
-            else if(sum < target) left++;
-            else right--;
-        }
-        return {};*/
-
-        map<int,int> mpp;
         int n = nums.size();
+        int l = 0;
+        int r = n-1;
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+        while(l < r) {
+            int sum = sorted[l] + sorted[r];
+            if(sum == target) break;
+            else if(sum < target) l++;
+            else r--;
+        } 
+        vector<int> result;
         for(int i=0; i<n; i++) {
-            int num = nums[i];
-            int  moreNeeded = target - num;
-            if(mpp.find(moreNeeded) != mpp.end()) {//mpp.find(moreNeeded) gives the iterator pointing to the given number if found and if not found it points to mpp.end().
-            //in this case agr mpp.find(moreNeeded) is not equal to mpp.end() mtlb anth tk nhi phoncha usse phle ruk gya mtlb mil gya iterator so hmlog return kr denge
-            
-                return {mpp[moreNeeded], i};//agr mil gya toh return kro dono ka indices
+            if(nums[i] == sorted[l] || nums[i] == sorted[r]) {
+                result.push_back(i);
             }
-            mpp[num] = i;// nhi toh assign kr do
-
         }
-        return {-1, -1};
 
+        return result;
     }
 };
