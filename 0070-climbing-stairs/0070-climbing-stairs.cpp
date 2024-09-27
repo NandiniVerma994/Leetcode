21ddd41a61aad062240be248ea9a1e99560e4a77
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2024-09-27 17:54:13 +0530

    Time: 0 ms (100%), Space: 7.6 MB (38.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int n, vector<int> &dp) {
+    /*int solve(int n, vector<int> &dp) {
         if(n < 0) {
             return 0;
         }
@@ -20,5 +20,22 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
         return solve(n, dp);
+    }*/
+
+    //BOTTOM UP
+    int climbStairs(int n) {
+        if(n == 1 || n == 2 || n == 3) {
+            return n;
+        }
+
+        vector<int> arr(n+1);
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for(int i=3; i<=n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
     }
 };
