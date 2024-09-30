5fef7298a06739c59efd65429104e8afe439e4d9
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2024-09-30 20:01:23 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -18,8 +18,26 @@ public:
         ans.push_back(root->val);
     }
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        postOrder(root, ans);
-        return ans;
+        vector<int> postOrder;
+        stack<TreeNode*> st1, st2;
+        if(root == NULL) return postOrder;
+        st1.push(root);
+        while(!st1.empty()) {
+            root = st1.top();
+            st1.pop();
+            //everytime while iterating push 1 into stack2 and then check for left and right
+            st2.push(root);
+            if(root->left != NULL) {
+                st1.push(root->left);
+            }
+            if(root->right != NULL) {
+                st1.push(root->right);
+            }
+        }
+        while(!st2.empty()) {
+            postOrder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postOrder;
     }
 };
