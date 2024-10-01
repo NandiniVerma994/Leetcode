8ee894927bf720c6a2460a2958901814e5fa3fe3
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2024-10-01 17:47:07 +0530

    Time: 4 ms (32.23%), Space: 9.5 MB (63.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //MEMOISATION
+    /*
     int solve(vector<int>& nums, int i, int n, vector<int> &dp) {
         if(i >= n) {
             return 0;
@@ -16,5 +18,21 @@ public:
         int n = nums.size();
         vector<int> dp(101, -1);//size is till 100
         return solve(nums, 0,n, dp);
+    }*/
+    //BOTTOM UP
+    int rob(vector<int> &nums) {
+        int n = nums.size();
+        
+        vector<int> dp(101, 0);
+        //dp[i] is the maximum stolen money till house i
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i=2; i<=n; i++) {
+            int steal = nums[i-1] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = max(steal, skip);
+        }
+        return dp[n];
     }
 };
