9359ff010e325b2e66e6f0f033365db0484df0f3
Nandini Kumari <145362258+NandiniVerma994@users.noreply.github.com>
2024-10-01 19:43:43 +0530

    Time: 4 ms (33.06%), Space: 9.7 MB (36.46%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int> &nums, int i, int n) {
-        if(i > n) return 0;
-        if(t[i] != -1) {
-            return t[i];
-        }
-        int steal = nums[i] + solve(nums, i+2, n);
-        int skip = solve(nums, i+1, n);
-        return t[i] = max(steal, skip);
-    }
-
-    int rob(vector<int>& nums) {
+    //BOTTOM UP
+    int rob(vector<int> &nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
-        if(n == 2) return (max(nums[0], nums[1]));
-        memset(t, -1, sizeof(t));
-        int take_0thIndex_house = solve(nums, 0, n-2);
-        memset(t, -1, sizeof(t));
-        int take_1stIndex_house = solve(nums, 1, n-1);
-        return max(take_0thIndex_house, take_1stIndex_house);
+        vector<int> t(n+1, 0);
+        if(n == 1) {
+            return nums[0];
+        }
+        //t[i] is the money stolen when you have i houses
+        t[0] = 0;
+        //taking first house means skipping the last house
+        for(int i=1; i<=n-1; i++) {
+            int skip = t[i-1];
+            int steal = nums[i-1] + ((i-2 >= 0) ? t[i-2] : 0);
+            t[i] = max(skip, steal);
+        }
+        int result1 = t[n-1];
+        t.clear();
+
+        //skipping first house
+        t[0] = 0;
+        t[1] = 0;
+        for(int i=2; i<=n; i++) {
+            int skip = t[i-1];
+            int steal = nums[i-1] + ((i-2 >= 0) ? t[i-2] : 0);
+            t[i] = max(skip, steal);
+        }
+        int result2 = t[n];
+        return max(result1, result2);
     }
 };
